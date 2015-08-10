3202aef5c3d278f6c7f481985730ddb1b3f6f15c
Nan Wang <nantas@gmail.com>
2015-08-10 11:28:29 +0800

    remove texture reference in Tilemap demo.

--- assets/demos/tilemap_demo/TileMapDemo.js
+++ assets/demos/tilemap_demo/TileMapDemo.js
@@ -1,10 +1,10 @@
 var TileMapDemo = Fire.Class({
     extends: Fire.Behavior,
     properties: {
-        tilemapImage: { // current work around for preload tilemap texture
-            default: "",
-            url: Fire.Texture
-        }
+        // tilemapImage: { // current work around for preload tilemap texture
+        //     default: "",
+        //     url: Fire.Texture
+        // }
     },
     onLoad: function() {
         setResolution();
