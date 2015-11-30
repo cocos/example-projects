cdc383b0b694fe4f8d262a2604739ac1e0ce0a25
Nan Wang <nantas@gmail.com>
2015-11-30 09:38:46 +0800

    add sprite animation case.

---
+++ assets/cases/gameplay/Sheep_Animation_1.js
@@ -0,0 +1,32 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // sheepAnim: {
+        //     default: null,
+        //     type: cc.ENode
+        // }
+        // foo: {
+        //    default: null,
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        var anim = this.getComponent(cc.AnimationComponent);
+        setTimeout(function() {
+            anim.play('sheep_jump');
+        }, 2000);
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
