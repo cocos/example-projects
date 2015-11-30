4fdfeee87874af7faf76b1d234cb389bcae4a84f
Nan Wang <nantas@gmail.com>
2015-11-30 11:16:48 +0800

    add keyboard input control, need cc.eventManager support.

--- assets/cases/gameplay/SimpleKeyboardMovement.js
+++ assets/cases/gameplay/SimpleKeyboardMovement.js
@@ -2,6 +2,10 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        sheep: {
+            default: null,
+            type: cc.ENode
+        }
         // foo: {
         //    default: null,
         //    url: cc.Texture2D,  // optional, default is typeof default
@@ -15,11 +19,33 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
+        var self = this;
 
+        // set initial move direction
+        self.turnRight();
+
+        // delete this when integrated keyboard control
+        setTimeout(function() {
+            // turn left
+            self.turnLeft();
+        }, 1000);
+
+        //TODO: add keyboard input listener to call turnLeft and turnRight
+        // cc.eventManager.addListener();
     },
 
     // called every frame
     update: function (dt) {
-
+        this.sheep.x += this.speed * dt;
     },
+
+    turnLeft: function() {
+        this.speed = -100;
+        this.sheep.scaleX = 1;
+    },
+
+    turnRight: function() {
+        this.speed = 100;
+        this.sheep.scaleX = -1;
+    }
 });
