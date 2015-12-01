16485142dae158b9114897d0d899c9c96b12d8c1
Nan Wang <nantas@gmail.com>
2015-12-01 15:50:23 +0800

    complete simple action case, add label overflow.

--- assets/cases/gameplay/actions/SimpleAction.js
+++ assets/cases/gameplay/actions/SimpleAction.js
@@ -2,20 +2,23 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        jumper: {
+            default: null,
+            type: cc.ENode
+        }
     },
 
     // use this for initialization
     onLoad: function () {
-
+        this.squashAction = cc.scaleTo(0.2, 1, 0.6);
+        this.stretchAction = cc.scaleTo(0.2, 1, 1.2);
+        this.scaleBackAction = cc.scaleTo(0.1, 1, 1);
+        this.moveUpAction = cc.moveBy(1, cc.p(0, 200)).easing(cc.easeCubicActionOut());
+        this.moveDownAction = cc.moveBy(1, cc.p(0, -200)).easing(cc.easeCubicActionIn());
+        var seq = cc.sequence(this.squashAction, this.stretchAction, 
+            this.moveUpAction, this.scaleBackAction, this.moveDownAction, this.squashAction, this.scaleBackAction);
+        // this is a temp api which will be combined to cc.ENode
+        this.jumper._sgNode.runAction(seq);
     },
 
     // called every frame
