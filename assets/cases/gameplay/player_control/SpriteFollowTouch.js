3411ff6a6ed9b14f5f9546980cfa3345de6ebdc4
Nan Wang <nantas@gmail.com>
2015-12-01 11:59:57 +0800

    add complete keyboard and touch input event manager example cases.

--- assets/cases/gameplay/player_control/SpriteFollowTouch.js
+++ assets/cases/gameplay/player_control/SpriteFollowTouch.js
@@ -2,35 +2,51 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        touchLocationDisplay: {
+            default: null,
+            type: cc.ENode
+        },
         follower: {
             default: null,
             type: cc.ENode
-        }
-        // foo: {
-        //    default: null,
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        },
+        followSpeed: 0
     },
 
     // use this for initialization
     onLoad: function () {
         var self = this;
+        self.moveToPos = cc.p(0, 0);
+        self.isMoving = false;
         cc.eventManager.addListener({
             event: cc.EventListener.TOUCH_ONE_BY_ONE, 
             onTouchBegan: function(touch, event) {
                 var touchLoc = touch.getLocation();
                 console.log(touchLoc);
+                self.isMoving = true;
+                self.moveToPos = touchLoc;
+                return true; // don't capture event
+            },
+            onTouchMoved: function(touch, event) {
+                var touchLoc = touch.getLocation();
+                self.touchLocationDisplay.getComponent(cc.ELabel).string = 'touch (' + touchLoc.x + ', ' + touchLoc.y + ')';
+                self.moveToPos = touchLoc;
+            },
+            onTouchEnded: function(touch, event) {
+                self.isMoving = false; // when touch ended, stop moving
             }
-        }, self.node._sgNode);
+        }, self);
     },
 
     // called every frame
     update: function (dt) {
-
-    },
+        if (!this.isMoving) return;
+        var oldPos = this.follower.position;
+         // get move direction
+        var direction = cc.pNormalize(cc.pSub(this.moveToPos, oldPos));
+        // multiply direction with distance to get new position
+        var newPos = cc.pAdd(oldPos, cc.pMult(direction, this.followSpeed * dt)); 
+        // set new position
+        this.follower.setPosition(newPos);
+    }
 });
