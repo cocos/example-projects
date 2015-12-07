fbe1f8ffb538f133032215416f5e533cfdcb6311
Nan Wang <nantas@gmail.com>
2015-12-07 13:04:41 +0800

    add ttf font asset, add data cases.

---
+++ assets/cases/05_data/02_prefab/PopulatePrefab.js
@@ -0,0 +1,25 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        prefab: {
+            default: null,
+            type: cc.Node
+        },
+        numberToSpawn: 0
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this.randomRange = cc.p(400, 300);
+    },
+
+    getRandomPosition: function() {
+        return cc.p(cc.random0To1() * this.randomRange.x, cc.random0To1() * this.randomRange.y);
+    },
+
+    // called every frame
+    update: function (dt) {
+        // console.log(this.getRandomPosition());
+    },
+});
