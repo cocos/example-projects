392e0c3f071218ba808071a5835abec14491ec30
knoxHuang <821686174@qq.com>
2016-02-26 16:23:31 +0800

    add add example for Asset Loading

---
+++ assets/cases/05_scripting/07_asset_loading/ComeBackToAssetLoad.js
@@ -0,0 +1,29 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    onComeBlack: function () {
+        cc.director.loadScene("asset_loading.fire");
+    }
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
