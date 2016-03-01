e08296f49ae3c76f94af828591cf63facd1002c0
knoxHuang <821686174@qq.com>
2016-03-01 15:56:54 +0800

    Improve Move Animation

---
+++ assets/cases/03_gameplay/03_animation/MoveAnimationCtrl.js
@@ -0,0 +1,46 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        target: {
+            default: null,
+            type: cc.Animation
+        },
+
+        nodes: {
+            default: [],
+            type: cc.Node
+        }
+    },
+
+    onLoad: function () {
+        this.onRegisteredEvent();
+    },
+
+    onRegisteredEvent: function () {
+        for (var i = 0; i < this.nodes.length; ++i) {
+            this.nodes[i].on(cc.Node.EventType.MOUSE_DOWN, this.onPlayAnimation.bind(this));
+        }
+    },
+
+    onPlayAnimation: function (event) {
+        switch (event.target._name) {
+            case "Linear":
+                this.target.play("linear");
+                break;
+            case "CaseIn_Expo":
+                this.target.play("caseIn-expo");
+                break;
+            case "CaseOut_Expo":
+                this.target.play("caseInOut-expo");
+                break;
+            case "CaseInOut_Expo":
+                this.target.play("caseOut-expo");
+                break;
+            case "Back_Forward":
+                this.target.play("back-forward");
+                break;
+        }
+    }
+
+});
