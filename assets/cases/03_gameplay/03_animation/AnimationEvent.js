c2468253468d91d52f18334678117a28c447837f
knoxHuang <821686174@qq.com>
2016-03-01 16:44:57 +0800

    add example for Animation Event

--- assets/cases/03_gameplay/03_animation/AnimationEvent.js
+++ assets/cases/03_gameplay/03_animation/AnimationEvent.js
@@ -2,23 +2,17 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
-    },
-    
-    moveEnd: function () {
-        this.node.runAction(cc.sequence(cc.scaleTo(0.05, 2, 2), cc.scaleTo(0.05, 1, 1)));
+
     },
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+    onLoad: function () {
+        var node = cc.find('Canvas/New Label');
+        this._label = node.getComponent(cc.Label);
+        this._animCtrl = this.node.getComponent(cc.Animation);
+    },
 
-    // },
+    onNextAnimation: function (step) {
+        this._animCtrl.play("step_"+ step);
+        this._label.string = "开始第"+ step + "个动画";
+    }
 });
