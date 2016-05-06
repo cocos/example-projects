7502d16e77b39c617823fcd8973d115e8a400470
knoxHuang <821686174@qq.com>
2016-05-06 18:35:36 +0800

    add MotionStreak Example

---
+++ assets/cases/motionStreak/motionStreakCtrl.js
@@ -0,0 +1,39 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        motionStreak: {
+            default: null,
+            type: cc.MotionStreak
+        },
+
+        newTexture: {
+            default: null,
+            url: cc.Texture2D
+        }
+    },
+
+    onLoad: function () {
+        this._changed = true;
+        this.oldTexture = this.motionStreak.texture;
+    },
+
+    onClick: function () {
+        if (this._changed) {
+            this.setMotionStreak(2, 3, 20, this.newTexture);
+        }
+        else {
+            this.setMotionStreak(0.5, 1, 30, this.oldTexture);
+        }
+        this._changed = !this._changed;
+    },
+
+    setMotionStreak: function (fadeTime, minSeg, stroke, texture) {
+        this.motionStreak.fadeTime = fadeTime;
+        this.motionStreak.minSeg = minSeg;
+        this.motionStreak.stroke = stroke;
+        this.motionStreak.texture = texture;
+    }
+
+
+});
