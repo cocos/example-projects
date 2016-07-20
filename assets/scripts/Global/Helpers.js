47a2fbdc690185e36638f36a693e9537bf895847
Jare Guo <jare85@163.com>
2016-07-20 14:06:53 +0800

    try to fix runtime glitter

--- assets/scripts/Global/Helpers.js
+++ assets/scripts/Global/Helpers.js
@@ -1,6 +1,12 @@
+if (CC_JSB && cc.runtime) {
+    // fix cocos-creator/fireball#3578
+    cc.LoaderLayer.setUseDefaultSource(false);
+    cc.Dialog.setUseDefaultSource(false);
+}
+
 // Returns a random integer between min (included) and max (excluded)
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 module.exports = {
