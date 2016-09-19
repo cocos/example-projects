4a90c0e3527810c0fbeb44dca430b55780148108
zilongshanren <guanghui8827@126.com>
2016-09-19 16:32:34 +0800

    add checkBox examples

---
+++ assets/cases/02_ui/13_checkbox/checkbox.js
@@ -0,0 +1,63 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        checkbox: {
+            default: null,
+            type: cc.Toggle
+        },
+
+        checkBoxEventString: {
+            default: null,
+            type: cc.Label
+        },
+
+        radioButtonEventString: {
+            default: null,
+            type: cc.Label
+        },
+
+        radioButton: {
+            default: [],
+            type: cc.Toggle
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        if(this.checkbox) {
+            this._updateToggleEventString("CheckBox", this.checkBoxEventString, this.checkbox);
+        }
+    },
+
+    _updateToggleEventString: function(title, label, toggle) {
+        if(toggle.isChecked) {
+            label.string = title + " is checked.";
+        } else {
+            label.string = title + " is unchecked.";
+        }
+    },
+
+    radioButtonClicked: function(event, toggle) {
+        var index = this.radioButton.indexOf(toggle);
+        var title = "RadioButton";
+        switch(index) {
+          case 0:
+              title += "1";
+              break;
+          case 1:
+              title += "2";
+              break;
+          case 2:
+              title += "3";
+              break;
+          default:
+              break;
+        }
+        this._updateToggleEventString(title, this.radioButtonEventString, toggle);
+    },
+
+    checkBoxClicked: function (event, toggle) {
+        this._updateToggleEventString("CheckBox", this.checkBoxEventString, toggle);
+    }
+});
