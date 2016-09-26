af561616d317a815c5ee2c1883e6c0d4f5c8351b
knoxHuang <821686174@qq.com>
2016-09-26 15:49:59 +0800

    update player_control

---
+++ assets/cases/03_gameplay/01_player_control/KeyboardInput/SimpleKeyboardMovement.js
@@ -0,0 +1,49 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        sheep: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // use this for initialization
+    onLoad () {
+        // set initial move direction
+        this.turnRight();
+
+        //add keyboard input listener to call turnLeft and turnRight
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown.bind(this), this.nodes);
+    },
+
+    onKeyDown (event) {
+        switch(event.keyCode) {
+            case cc.KEY.a:
+            case cc.KEY.left:
+                console.log('turn left');
+                this.turnLeft();
+                break;
+            case cc.KEY.d:
+            case cc.KEY.right:
+                console.log('turn right');
+                this.turnRight();
+                break;
+        }
+    },
+
+    // called every frame
+    update (dt) {
+        this.sheep.x += this.speed * dt;
+    },
+
+    turnLeft () {
+        this.speed = -100;
+        this.sheep.scaleX = 1;
+    },
+
+    turnRight () {
+        this.speed = 100;
+        this.sheep.scaleX = -1;
+    }
+});
