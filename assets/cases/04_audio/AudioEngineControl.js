6c0bcde7e002db35dc9f4adf08b547bdcc52082e
VisualSJ <itharbors@outlook.com>
2016-09-29 13:42:40 +0800

    Update audio test

---
+++ assets/cases/04_audio/AudioEngineControl.js
@@ -0,0 +1,55 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        audio: {
+            url: cc.AudioClip,
+            default: null
+        },
+        
+        label: {
+            type: cc.Label,
+            default: null
+        }
+    },
+
+    onLoad: function () {
+        this.maxNum = cc.audioEngine.getMaxAudioInstance();
+        this.audioPool = [];
+    },
+
+    update: function () {
+        if (!this.label) return;
+        for (var i=0; i<this.audioPool.length; i++) {
+            var id = this.audioPool[i];
+            var state = cc.audioEngine.getState(id);
+            if (state < 0) {
+                this.audioPool.splice(i, 1);
+                i--;
+            }
+        }
+        this.label.string = 'Instance: ' + this.audioPool.length + ' / ' + this.maxNum;
+    },
+    
+    play: function () {
+        if (!this.audio) return;
+        var id = cc.audioEngine.play(this.audio, false, 1);
+        cc.audioEngine.setCurrentTime(id, 70);
+        this.audioPool.push(id);
+    },
+    
+    stopAll: function () {
+        if (!this.audio) return;
+        cc.audioEngine.stopAll();
+    },
+    
+    pauseAll: function () {
+        if (!this.audio) return;
+        cc.audioEngine.pauseAll();
+    },
+    
+    resumeAll: function () {
+        if (!this.audio) return;
+        cc.audioEngine.resumeAll();
+    },
+});
---
+++ assets/cases/04_audio/AudioSourceControl.js
@@ -0,0 +1,45 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        audioSource: {
+            type: cc.AudioSource,
+            default: null
+        },
+        
+        label: {
+            type: cc.Label,
+            default: null
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        // cc.audioEngine.setMaxWebAudioSize(1024*10);
+    },
+    
+    update: function () {
+        if (!this.label) {
+            return;
+        }
+        var audio = this.audioSource;
+        this.label.string = audio.getCurrentTime().toFixed(1) + ' s / ' + audio.getDuration().toFixed(1) + ' s';
+    },
+    
+    play: function () {
+        this.audioSource.play();
+        this.audioSource.setCurrentTime(70);
+    },
+    
+    pause: function () {
+        this.audioSource.pause();
+    },
+    
+    stop: function () {
+        this.audioSource.stop();
+    },
+    
+    resume: function () {
+        this.audioSource.resume();
+    }
+});
