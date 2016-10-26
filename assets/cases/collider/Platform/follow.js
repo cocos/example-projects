9b50b3bb1b2c375e049c445e165c02baa9a55377
knoxHuang <821686174@qq.com>
2016-10-26 12:43:54 +0800

    add tips and fix bug

--- assets/cases/collider/Platform/follow.js
+++ assets/cases/collider/Platform/follow.js
@@ -10,10 +10,12 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
+        // 由于需要键盘操作所以只能在 PC 才可用
+        this.node.active = !cc.sys.isMobile;
+
         if (!this.target) {
             return;
         }
-
         var follow = cc.follow(this.target, cc.rect(0,0, 2000,2000));
         this.node.runAction(follow);
     }
