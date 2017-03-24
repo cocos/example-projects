99c567b47a04b00d9dfbad1dd1454c162e7e856a
pandamicro <lphuabin@gmail.com>
2017-03-24 14:20:14 +0800

    Fix loadingBar releasing other resources issue

--- assets/cases/05_scripting/10_loadingBar/LoadingBarCtrl.js
+++ assets/cases/05_scripting/10_loadingBar/LoadingBarCtrl.js
@@ -31,9 +31,9 @@ cc.Class({
             cc.url.raw("resources/audio/cheering.wav"),
             cc.url.raw("resources/audio/music_logo.mp3"),
             cc.url.raw("resources/test assets/audio.mp3"),
-            cc.url.raw("resources/test assets/font.png"),
-            cc.url.raw("resources/font/mikado_outline_shadow.png"),
-            cc.url.raw("resources/font/enligsh-chinese.png")
+            cc.url.raw("resources/loadingBar/font.png"),
+            cc.url.raw("resources/loadingBar/mikado_outline_shadow.png"),
+            cc.url.raw("resources/loadingBar/enligsh-chinese.png")
         ];
         this.resource = null;
         this.progressBar.progress = 0;
