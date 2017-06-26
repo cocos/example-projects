0a8c524c4dba36a37b951f347edf2ee531f74480
pandamicro <lphuabin@gmail.com>
2017-06-26 14:41:30 +0800

    Enhance NodePool test case

--- assets/cases/05_scripting/12_pool/PoolHandler.js
+++ assets/cases/05_scripting/12_pool/PoolHandler.js
@@ -1,22 +1,40 @@
+var lastClick = 0;
+
 function pauseresume () {
-    if (this.paused) {
-        cc.director.getActionManager().resumeTarget(this);
+    var now = Date.now();
+    // Double click in 1 second
+    if (now - lastClick < 1000) {
+        this.stopAllActions();
+        var pool = this.getComponent('PoolHandler')._pool;
+        if (pool) {
+            pool.put(this);
+        }
+        else {
+            this.removeFromParent(true);
+        }
     }
+    // Click
     else {
-        cc.director.getActionManager().pauseTarget(this);
+        if (this.paused) {
+            cc.director.getActionManager().resumeTarget(this);
+        }
+        else {
+            cc.director.getActionManager().pauseTarget(this);
+        }
+        this.paused = !this.paused;
     }
-    this.paused = !this.paused;
+    lastClick = now;
 }
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
+        _pool: null
     },
     
     onLoad: function () {
-        this.node.paused = false;
-        this.node.on(cc.Node.EventType.TOUCH_END, pauseresume, this.node);
+        this.reuse();
     },
     
     unuse: function () {
@@ -24,6 +42,7 @@ cc.Class({
     },
     
     reuse: function () {
+        this.node.paused = false;
         this.node.on(cc.Node.EventType.TOUCH_END, pauseresume, this.node);
     }
 });
