74b99ddc0b8f955aa4475b242584d9279fbf5e42
knoxHuang <821686174@qq.com>
2017-08-04 17:37:18 +0800

    Add the Camera button according to the Render mode switch

--- assets/cases/culling/culling.js
+++ assets/cases/culling/culling.js
@@ -2,61 +2,57 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-       buttons: [cc.Node],
-	   cameraNode: {
-		   default: null,
-		   type: cc.Node
-	   },
-	   cameraButtonLabel: {
-		   default: null,
-		   type: cc.Label
-	   }
+        buttons: [cc.Node],
+        cameraNode: {
+            default: null,
+            type: cc.Node
+        },
+        cameraButtonLabel: {
+            default: null,
+            type: cc.Label
+        }
     },
 
     // use this for initialization
     onLoad: function () {
-    	cc.director.setDisplayStats(true);
-    	var g = this.getComponent(cc.Graphics);
-    	if(g) {
-	   		g.lineWidth = 10;
-	        g.fillColor = cc.hexToColor('#ff0000');
-	        
-	        g.moveTo(-20, 0);
-	        g.lineTo(0, -100);
-	        g.lineTo(20, 0);
-	        g.lineTo(0, 100);
-	        g.close();
+        // camera not support canvas
+        this.cameraButtonLabel.node.parent.active = cc._renderType !== cc.game.RENDER_TYPE_CANVAS;
+        cc.director.setDisplayStats(true);
+        var g = this.getComponent(cc.Graphics);
+        if (g) {
+            g.lineWidth = 10;
+            g.fillColor = cc.hexToColor('#ff0000');
 
-	        g.stroke();
-	        g.fill();
-    	}
+            g.moveTo(-20, 0);
+            g.lineTo(0, -100);
+            g.lineTo(20, 0);
+            g.lineTo(0, 100);
+            g.close();
 
-		// this.changeCamera();
+            g.stroke();
+            g.fill();
+        }
+        // this.changeCamera();
     },
 
     spawnGameObject: function (event, data) {
-    	cc.log("data : = " +　data)
-    	var node = this.buttons[data|0];
-    	node.runAction(cc.sequence(cc.moveBy(3, cc.p(1200,0)), cc.moveBy(3, cc.p(-1200,0))));
+        cc.log("data : = " + data);
+        var node = this.buttons[data | 0];
+        node.runAction(cc.sequence(cc.moveBy(3, cc.p(1200, 0)), cc.moveBy(3, cc.p(-1200, 0))));
     },
 
     onDestroy: function () {
-    	cc.director.setDisplayStats(false);
+        cc.director.setDisplayStats(false);
     },
 
-	changeCamera: function () {
-		if (this.cameraNode.active) {
-			this.cameraNode.active = false;
-			this.cameraButtonLabel.string = 'Enable Camera';
-		}
-		else {
-			this.cameraNode.active = true;
-			this.cameraButtonLabel.string = 'Disable Camera';
-		}
-	},
-
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
+    changeCamera: function () {
+        if (this.cameraNode.active) {
+            this.cameraNode.active = false;
+            this.cameraButtonLabel.string = 'Enable Camera';
+        }
+        else {
+            this.cameraNode.active = true;
+            this.cameraButtonLabel.string = 'Disable Camera';
+        }
+    }
 });
