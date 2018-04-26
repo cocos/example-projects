569e280918a8fc97f4e369289dad7e01ed219029
PP <chinese1236@qq.com>
2018-04-26 18:25:32 +0800

    add exmaple case supporting multi tsxFiles

---
+++ assets/resources/tilemap/tile_iso_offset_with_tsx1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tile_iso_offset_with_tsx1" tilewidth="101" tileheight="101" tilecount="2" columns="1">
+ <image source="tile_iso_offset_with_tsx1.png" width="101" height="202"/>
+</tileset>
