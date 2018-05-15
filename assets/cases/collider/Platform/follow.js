1b2db9c7998fa3b8995c445e938316f5715d8046
knox <knox.h@qq.com>
2018-05-15 10:35:09 +0800

    fix follow example error for fireball/issues/7286

--- assets/cases/collider/Platform/follow.js
+++ assets/cases/collider/Platform/follow.js
@@ -16,7 +16,7 @@ cc.Class({
         if (!this.target) {
             return;
         }
-        var follow = cc.follow(this.target, cc.rect(0,0, 2000,2000));
+        var follow = cc.follow(this.target, cc.rect(0, -5, 2000,2000));
         this.node.runAction(follow);
     }
 });
