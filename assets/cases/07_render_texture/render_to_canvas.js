0708b1385ed6488ce35899e7a8c4696bd75d9d22
yinkaile <501251991@qq.com>
2018-05-30 17:57:05 +0800

    fixed screen capture error on wechat platform

--- assets/cases/07_render_texture/render_to_canvas.js
+++ assets/cases/07_render_texture/render_to_canvas.js
@@ -34,8 +34,12 @@ cc.Class({
         let rowBytes = width * 4;
         for (let row = 0; row < height; row++) {
             let srow = height - 1 - row;
-            let data2 = new Uint8ClampedArray(data.buffer, srow * width * 4, rowBytes);
-            let imageData = new ImageData(data2, width, 1);
+            let imageData = ctx.createImageData(width, 1);
+            let start = srow*width*4;
+            for (let i = 0; i < rowBytes; i++) {
+                imageData.data[i] = data[start+i];
+            }
+
             ctx.putImageData(imageData, 0, row);
         }
 
@@ -47,18 +51,44 @@ cc.Class({
 
     captureAndShow () {
         var img = this.capture();
-        img.style.position = 'absolute';
-        img.style.display = 'block';
-        img.style.left = '0px'
-        img.style.top = '0px';
-        img.zIndex = 100;
 
-        img.onclick = function (event) {
-            event.stopPropagation();
-            img.remove();
-        }
+        // You can save the image or show it.
 
-        cc.game.container.appendChild(img);
+        // img.style.position = 'absolute';
+        // img.style.display = 'block';
+        // img.style.left = '0px'
+        // img.style.top = '0px';
+        // img.zIndex = 100;
+
+        // img.style.transform = cc.game.container.style.transform;
+        // img.style['transform-origin'] = cc.game.container.style['transform-origin'];
+        // img.style.margin = cc.game.container.style.margin;
+        // img.style.padding = cc.game.container.style.padding;
+
+        // img.onclick = function (event) {
+        //     event.stopPropagation();
+        //     img.remove();
+        // }
+
+        // document.body.appendChild(img);
+
+        let texture = new cc.Texture2D();
+        texture.initWithElement(img);
+
+        let spriteFrame = new cc.SpriteFrame();
+        spriteFrame.setTexture(texture);
+
+        let node = new cc.Node();
+        let sprite = node.addComponent(cc.Sprite);
+        sprite.spriteFrame = spriteFrame;
+
+        node.zIndex = cc.macro.MAX_ZINDEX;
+        node.parent = cc.director.getScene();
+        node.x = cc.winSize.width/2;
+        node.y = cc.winSize.height/2;
+        node.on(cc.Node.EventType.TOUCH_START, () => {
+            node.parent = null;
+        });
     }
 
     // update (dt) {},
