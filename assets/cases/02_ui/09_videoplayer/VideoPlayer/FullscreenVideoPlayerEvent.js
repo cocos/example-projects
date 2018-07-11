2481c5aa161ce85e778d21aff4000dc21517994f
knox <knox.h@qq.com>
2018-07-11 17:03:12 +0800

    update refine

--- assets/cases/02_ui/09_videoplayer/VideoPlayer/FullscreenVideoPlayerEvent.js
+++ assets/cases/02_ui/09_videoplayer/VideoPlayer/FullscreenVideoPlayerEvent.js
@@ -8,11 +8,7 @@ cc.Class({
         if (!videoPlayer) {
             videoPlayer = this.node.getComponent(cc.VideoPlayer);
         }
-        if (event === cc.VideoPlayer.EventType.CLICKED) {
-            videoPlayer.stop();
-            videoPlayer.enabled = false;
-        }
-        else if (event === cc.VideoPlayer.EventType.COMPLETED) {
+        if (event === cc.VideoPlayer.EventType.COMPLETED) {
             videoPlayer.isFullscreen = false;
             videoPlayer.node.removeFromParent();
         }
