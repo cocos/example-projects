3fe8c5be0ba3fc1480ef28a8fb730fea0014aa87
PP <chinese1236@qq.com>
2018-10-08 15:55:53 +0800

    add buttonInteractable Scene

---
+++ assets/cases/02_ui/03_button/ButtonTransition/ButtonTransition.js
@@ -0,0 +1,19 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        btn: cc.Button,
+    },
+    
+    onInteractable (event) {
+        this.btn.interactable = event.isChecked;
+    },
+
+    onColorTransition (event) {
+        this.btn.transition = cc.Button.Transition.COLOR;
+    },
+
+    onSpriteTransition (event) {
+        this.btn.transition = cc.Button.Transition.SPRITE;
+    },
+});
