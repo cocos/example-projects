251e746126cf05be19ea993b27e70f16c83d8158
Coco <crazypad@qq.com>
2018-11-16 14:00:58 +0800

    capture scene adjust

--- assets/cases/07_capture_texture/capture_to_web.js
+++ assets/cases/07_capture_texture/capture_to_web.js
@@ -3,23 +3,13 @@ cc.Class({
 
     start () {
         this.init();
-    },
-
-    captureAndShow () {
-        this.createSprite();
-        var img = this.initImage();
-        this.showSprite(img);
-        // download the pic as the file to your local
-        this.label.string = 'Showing the capture'
-        this.saveFile('capture_to_web.png', img.src);
-    },
-
-    saveFile (fileName, dataUrl) {
-        let a = document.createElement('a');
-        a.href = dataUrl;
-        a.download = fileName;
-        const event = document.createEvent('MouseEvents');
-        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        a.dispatchEvent(event);
+        // create capture
+        this.schedule(() => {
+            this.createSprite();
+            var img = this.initImage();
+            this.showSprite(img);
+            // download the pic as the file to your local
+            this.label.string = 'Showing the capture'
+        }, 1, 0);
     }
 });
