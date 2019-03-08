730f2c43b4db870ba9bf38e476fd3d0b640d1a95
2youyouo2 <501251991@qq.com>
2019-03-08 11:23:48 +0800

    refine code

--- assets/cases/03_gameplay/tween/tween-demo.js
+++ assets/cases/03_gameplay/tween/tween-demo.js
@@ -2,39 +2,39 @@
 const t = cc.tween;
 
 cc.Class({
-	extends: cc.Component,
+  extends: cc.Component,
 
-	properties: {
-		nodes: [cc.Node]
-	},
+  properties: {
+    nodes: [cc.Node]
+  },
 
-	start () {
-		let nodes = this.nodes;
-		for (let i = 0; i < nodes.length; i++) {
-			let node = nodes[i];
-			t(node)
-				// The defference delay should only eval once
-				.delay(0.5 + i * 0.2)
-				// repeat 1000 times
-				.repeat(
-					t()
-						// first reset node properties
-						.set({ opacity: 0, scale: 10, x: 0, rotation: 0 })
-						// parallel exec tween
-						.parallel(
-							t().to(1, { opacity: 255, scale: 1 }, { easing: 'quintInOut' }),
-							t().to(2.5, { x: node.x }, { easing: 'backOut' })
-						)
-						.delay(0.5)
-						.to(0.8, { rotation: 360 }, { easing: 'cubicInOut' })
-						.delay(1)
-						.to(0.3, { opacity: 0, scale: 3 }, { easing: "quintIn" })
-						.delay(1)
-					
-				, 1000)
-				.start()
-		}
-	},
+  start () {
+    let nodes = this.nodes;
+    for (let i = 0; i < nodes.length; i++) {
+      let node = nodes[i];
+      t(node)
+        // The defference delay should only eval once
+        .delay(0.5 + i * 0.2)
+        // repeat 1000 times
+        .repeat(
+          t()
+            // first reset node properties
+            .set({ opacity: 0, scale: 10, x: 0, rotation: 0 })
+            // parallel exec tween
+            .parallel(
+              t().to(1, { opacity: 255, scale: 1 }, { easing: 'quintInOut' }),
+              t().to(2.5, { x: node.x }, { easing: 'backOut' })
+            )
+            .delay(0.5)
+            .to(0.8, { rotation: 360 }, { easing: 'cubicInOut' })
+            .delay(1)
+            .to(0.3, { opacity: 0, scale: 3 }, { easing: "quintIn" })
+            .delay(1)
 
-	// update (dt) {},
+        , 1000)
+        .start()
+    }
+  },
+
+  // update (dt) {},
 });
