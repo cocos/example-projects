faabdf255f47f0d5ed11845fc92e258abfff8b12
2youyouo2 <501251991@qq.com>
2019-03-12 14:19:02 +0800

    update 3d model demo

---
+++ assets/cases/3d/3d-model.js
@@ -0,0 +1,24 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        playingIndex: 0,
+    },
+
+    start () {
+        this.playNextAnimation();
+    },
+
+    playNextAnimation () {
+        let animation = this.getComponent(cc.Animation);
+        let clips = animation.getClips();
+        if (!clips[this.playingIndex]) {
+            this.playingIndex = 0;
+        }
+
+        animation.play(clips[this.playingIndex].name);
+        this.playingIndex++;
+    }
+
+});
