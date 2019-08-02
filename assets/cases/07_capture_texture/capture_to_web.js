748d2936e7f005d930c674342cf884481d27800d
engine <3503715604@qq.com>
2019-08-02 09:49:27 +0800

    bug fix: failed to capture image

--- assets/cases/07_capture_texture/capture_to_web.js
+++ assets/cases/07_capture_texture/capture_to_web.js
@@ -4,9 +4,9 @@ cc.Class({
     start () {
         this.init();
         // create capture
+        this.createCanvas();
+        var img = this.createImg();
         this.scheduleOnce(() => {
-            this.createCanvas();
-            var img = this.createImg();
             this.showImage(img);
             // download the pic as the file to your local
             this.label.string = 'Showing the capture'
