a7c48a8aafbea4efb12ff0eafaba7911350e8b27
owen <1053210246@qq.com>
2019-09-26 19:14:51 +0800

    fix tiledmap change scene throw error (#770)

--- assets/cases/tiledmap/ShieldNode.js
+++ assets/cases/tiledmap/ShieldNode.js
@@ -2,21 +2,26 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        prefab:{
-            type: cc.Prefab,
-            default: null,
-        },
-
         tiledLayer: {
             type: cc.TiledLayer,
             default: null,
         },
     },
 
-    start () {
+    onLoad () {
+        cc.loader.loadRes("tilemap/shieldNode", function (err, prefab) {
+            if (err) {
+                cc.error("tilemap/shieldNode resources load failed");
+                return;
+            }
+            this.initScene(prefab);
+        }.bind(this));
+    },
+
+    initScene (prefab) {
         let posArr = [cc.v2(-249, 96), cc.v2(-150, 76), cc.v2(-60, 54), cc.v2(-248, -144), cc.v2(-89, -34)];
         for (let i = 0; i < posArr.length; i++) {
-            let shieldNode = cc.instantiate(this.prefab);
+            let shieldNode = cc.instantiate(prefab);
             shieldNode.x = posArr[i].x;
             shieldNode.y = posArr[i].y;
             this.tiledLayer.addUserNode(shieldNode);
