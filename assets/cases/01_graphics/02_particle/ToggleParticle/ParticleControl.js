977831f08beb4202c5df814dcd524ec0261b19c4
zhefeng.zhang <35944775+Jno1995@users.noreply.github.com>
2019-10-17 18:01:17 +0800

    Update ParticleControl.js

--- assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
+++ assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
@@ -4,7 +4,7 @@ cc.Class({
     properties: {
         particle: cc.ParticleSystem,
         particle2: cc.ParticleSystem,
-        _playing: false
+        _playing: false  //Record whether the particles are playing
     },
 
     toggleParticlePlay: function() {
@@ -15,11 +15,13 @@ cc.Class({
             myParticle.resetSystem(); // restart particle system
         }
     },
-
+    
     lateUpdate () {
         if (!this._playing) {
             this._playing = true;
+            //Execute at the next frame
             this.scheduleOnce(()=>{
+                // play particle
                 this.particle && this.particle.getComponent(cc.ParticleSystem).resetSystem();
                 this.particle2 && this.particle2.getComponent(cc.ParticleSystem).resetSystem();
             });
