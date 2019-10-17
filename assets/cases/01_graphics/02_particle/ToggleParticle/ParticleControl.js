ec2fdb7b31ca0023e90ff0796369a73ebfada2e5
Jno <35944775+Jno1995@users.noreply.github.com>
2019-10-17 17:38:45 +0800

    02_particle scenes optimize

--- assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
+++ assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
@@ -2,15 +2,27 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        particle: cc.Node,
+        particle: cc.ParticleSystem,
+        particle2: cc.ParticleSystem,
+        _playing: false
     },
 
     toggleParticlePlay: function() {
-        var myParticle = this.particle.getComponent(cc.ParticleSystem);
+        var myParticle = this.particle;
         if (myParticle.particleCount > 0) { // check if particle has fully plaed
             myParticle.stopSystem(); // stop particle system
         } else {
             myParticle.resetSystem(); // restart particle system
+        }
+    },
+
+    lateUpdate () {
+        if (!this._playing) {
+            this._playing = true;
+            this.scheduleOnce(()=>{
+                this.particle && this.particle.getComponent(cc.ParticleSystem).resetSystem();
+                this.particle2 && this.particle2.getComponent(cc.ParticleSystem).resetSystem();
+            });
         }
     }
 });
