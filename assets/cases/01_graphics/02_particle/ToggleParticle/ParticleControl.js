1128bac23af6b07996be0e9197cbe9b3a9cbe8e4
zhefeng.zhang <35944775+Jno1995@users.noreply.github.com>
2019-10-18 09:29:24 +0800

    02_particle scenes optimize (#783)
    * 02_particle scenes optimize
    
    * Update ParticleControl.js

--- assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
+++ assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
@@ -2,15 +2,29 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        particle: cc.Node,
+        particle: cc.ParticleSystem,
+        particle2: cc.ParticleSystem,
+        _playing: false  //Record whether the particles are playing
     },
 
     toggleParticlePlay: function() {
-        var myParticle = this.particle.getComponent(cc.ParticleSystem);
+        var myParticle = this.particle;
         if (myParticle.particleCount > 0) { // check if particle has fully plaed
             myParticle.stopSystem(); // stop particle system
         } else {
             myParticle.resetSystem(); // restart particle system
+        }
+    },
+    
+    lateUpdate () {
+        if (!this._playing) {
+            this._playing = true;
+            //Execute at the next frame
+            this.scheduleOnce(()=>{
+                // play particle
+                this.particle && this.particle.getComponent(cc.ParticleSystem).resetSystem();
+                this.particle2 && this.particle2.getComponent(cc.ParticleSystem).resetSystem();
+            });
         }
     }
 });
