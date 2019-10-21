c4bfb02dd558e2063ef55b18f82e2834c641e38a
Jno <35944775+Jno1995@users.noreply.github.com>
2019-10-21 21:05:58 +0800

    recall #783 pr

--- assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
+++ assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
@@ -2,9 +2,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        particle: cc.ParticleSystem,
-        particle2: cc.ParticleSystem,
-        _playing: false  //Record whether the particles are playing
+        particle: cc.ParticleSystem
     },
 
     toggleParticlePlay: function() {
@@ -15,16 +13,4 @@ cc.Class({
             myParticle.resetSystem(); // restart particle system
         }
     },
-    
-    lateUpdate () {
-        if (!this._playing) {
-            this._playing = true;
-            //Execute at the next frame
-            this.scheduleOnce(()=>{
-                // play particle
-                this.particle && this.particle.getComponent(cc.ParticleSystem).resetSystem();
-                this.particle2 && this.particle2.getComponent(cc.ParticleSystem).resetSystem();
-            });
-        }
-    }
 });
