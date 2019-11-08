a3305937ab7920677911605d894a131c169c2cbe
zhefeng.zhang <35944775+Jno1995@users.noreply.github.com>
2019-11-08 13:56:12 +0800

    fixed #783pr error commit (#790)

--- assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
+++ assets/cases/01_graphics/02_particle/ToggleParticle/ParticleControl.js
@@ -3,8 +3,6 @@ cc.Class({
 
     properties: {
         particle: cc.ParticleSystem,
-        particle2: cc.ParticleSystem,
-        _playing: false  //Record whether the particles are playing
     },
 
     toggleParticlePlay: function() {
@@ -13,18 +11,6 @@ cc.Class({
             myParticle.stopSystem(); // stop particle system
         } else {
             myParticle.resetSystem(); // restart particle system
-        }
-    },
-    
-    lateUpdate () {
-        if (!this._playing) {
-            this._playing = true;
-            //Execute at the next frame
-            this.scheduleOnce(()=>{
-                // play particle
-                this.particle && this.particle.getComponent(cc.ParticleSystem).resetSystem();
-                this.particle2 && this.particle2.getComponent(cc.ParticleSystem).resetSystem();
-            });
         }
     }
 });
